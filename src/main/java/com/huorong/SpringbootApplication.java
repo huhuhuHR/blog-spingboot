466ba098d52c44cb717ccddfe8b8100867203535
huhuhuHR <757033301@qq.com>
2017-07-04 19:59:58 +0800

    整合mybatis

--- src/main/java/com/huorong/SpringbootApplication.java
+++ src/main/java/com/huorong/SpringbootApplication.java
@@ -1,9 +1,12 @@
 package com.huorong;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// mapper 接口类扫描包配置
+@MapperScan("com.huorong.dao")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/huorong/dao/CityDao.java
@@ -0,0 +1,21 @@
+package com.huorong.dao;
+
+
+import org.apache.ibatis.annotations.Param;
+
+import com.huorong.domain.City;
+
+/**
+ * 城市 DAO 接口类
+ *
+ * Created by bysocket on 07/02/2017.
+ */
+public interface CityDao {
+
+    /**
+     * 根据城市名称，查询城市信息
+     *
+     * @param cityName 城市名
+     */
+    City findByName(@Param("cityName") String cityName);
+}
